97ae78a11e1059b41c8986dab9036b3e7e945ee8
Sudheerkumar Chedella <sudheer.chedella@gmail.com>
2022-03-09 20:34:23 -0500

    Recent Test

---
+++ src/main/java/com/test/AnuTest.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class AnuTest {
+	@Test
+	public void orangeHRM()
+	{
+		System.setProperty("webdriver.chrome.driver", ".//drivers//chromedriver.exe");
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		driver.manage().window().maximize();
+		System.out.println(driver.getTitle());
+	}
+
+}
